5be7e8eb30c8836ab725e9e8eacf7f3adf8f80d8
pollseed <cb.so.devope+0001@gmail.com>
2015-01-29 02:31:45 +0900

    Update cmd.sh

--- web/ruby/cmd.sh
+++ web/ruby/cmd.sh
@@ -5,6 +5,7 @@ echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
 # ruby
+yum install gcc make openssl-devel
 rbenv install -l
 rbenv install 2.2.0
 rbenv global 2.2.0
