0e62ef1145f8226c7b9f4ce9957cfb1e9a1b6c45
pollseed <cb.so.devope+0001@gmail.com>
2015-01-29 02:30:24 +0900

    Create cmd.sh

---
+++ web/ruby/cmd.sh
@@ -0,0 +1,11 @@
+# rbenv
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+# ruby
+rbenv install -l
+rbenv install 2.2.0
+rbenv global 2.2.0
+ruby -v
