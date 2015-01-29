4c0417f60f41e34692c82265ca39c6f09ec45c8e
pollseed <cb.so.devope+0001@gmail.com>
2015-01-29 09:06:57 +0900

    じぇいそんにしたい

--- web/ruby/rails_setting_cmd.sh
+++ web/ruby/rails_setting_cmd.sh
@@ -6,8 +6,9 @@ git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+source ~/.bash_profile
 {$OS}
-## Macには、Ruby2.0が初期で入っているので特に理由がなければ入れない
+## MacはRuby2.0なので特に理由がなければ入れない
 rbenv install -l
 rbenv install 2.2.0
 rbenv global 2.2.0
@@ -15,12 +16,12 @@ rbenv rehash
 ruby -v
 
 {$OS} <- linux
-yum install gcc make openssl-devel
+  yum install gcc make openssl-devel
 
 {$OS} <- mac
-ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
-brew update
-brew install rbenv ruby-build
+  ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+  brew update
+  brew install rbenv ruby-build
 
 # Xcode Command-Line tools
 xcode-select --install
